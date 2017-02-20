ca0b182f241314b4b4a7930b833c3e8d43b908cf
kingcos <2821836721v@gmail.com>
2017-02-20 14:53:18 +0800

    HackingWithSwift - 03 - Add share menu

--- HackingWithSwift/01-ImageViewer/01-ImageViewer/MMImageViewController.swift
+++ HackingWithSwift/01-ImageViewer/01-ImageViewer/MMImageViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class MMImageViewController: UIViewController {
     
@@ -17,12 +18,32 @@ class MMImageViewController: UIViewController {
         super.viewDidLoad()
 
         title = imageName
-        
+        // 导航器右上按钮
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action,
+                                                            target: self,
+                                                            action: #selector(rightShareButtonClick))
         if let image = imageName {
             imageView.image = UIImage(named: image)
         }
     }
 
+    func rightShareButtonClick() {
+        // UIActivityViewController
+//        let activityVC = UIActivityViewController(activityItems: [imageView.image!],
+//                                                  applicationActivities: [])
+//        activityVC.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+//        present(activityVC, animated: true)
+        
+        // 分享到 iOS 内置社交媒体：需要导入 Social
+        if let slcVC = SLComposeViewController(forServiceType: SLServiceTypeSinaWeibo) {
+            slcVC.setInitialText("测试分享新浪微博")
+            slcVC.add(imageView.image!)
+            slcVC.add(URL(string: "maimieng.com"))
+            
+            present(slcVC, animated: true)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
